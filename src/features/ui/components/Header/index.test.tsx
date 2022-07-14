2700cd65f44d90e8ba4a2dccaebde759b5f8cc6c
Odusseus55 <odusseus.development@gmail.com>
2022-07-14 18:53:18 +0200

    test: header snapshot test added

---
+++ src/features/ui/components/Header/index.test.tsx
@@ -0,0 +1,18 @@
+// import { render, screen } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+
+import { Header } from '.'
+
+describe('[UI] Header', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Header />).toJSON()
+
+    expect(tree).toMatchSnapshot()
+
+    // console.log(screen.getAllByDisplayValue('About'))
+    // expect(screen.getAllByDisplayValue('About'))
+    // expect(screen.getByRole("heading", { name: /location search/i })
+    //     ).toBeVisible();
+    // console.log('test running')
+  })
+})
