2a9fa149ef7d3f688b847194c7971a710047e629
Odusseus55 <odusseus.development@gmail.com>
2022-07-17 10:32:23 +0200

    test: header test changed

--- src/features/ui/components/Header/index.test.tsx
+++ src/features/ui/components/Header/index.test.tsx
@@ -1,25 +1,16 @@
-// import { render, screen } from '@testing-library/react'
-import renderer from 'react-test-renderer'
+import { render, screen } from '@testing-library/react'
+
+import '@testing-library/jest-dom'
 
 import { Header } from '.'
 
 describe('[UI] Header', () => {
-  it('should match snapshot', () => {
-    const tree = renderer.create(<Header />).toJSON()
+  it('should render header', () => {
+    render(<Header />)
 
-    expect(tree).toMatchSnapshot()
-
-    // console.log(screen.getAllByDisplayValue('About'))
-    // expect(screen.getAllByDisplayValue('About'))
-    // expect(screen.getByRole("heading", { name: /location search/i })
-    //     ).toBeVisible();
-    // console.log('test running')
+    const header = screen.getByRole('navigation')
+    expect(header).toHaveTextContent('Recent Articles')
+    expect(header).toHaveTextContent('About')
+    expect(screen.getByText('Log in').closest('a')).toHaveTextContent('Log in')
   })
-
-  // it('should render header', () => {
-  //   render(<Header />)
-
-  //   console.log(screen.getByRole('navigation'))
-  //   expect(screen.getByRole('navigation')).toHaveTextContent(/Recent Articles/)
-  // })
 })
