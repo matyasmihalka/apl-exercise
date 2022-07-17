23c1e6442dd3484afafc702f9ec1d0020120e4df
Odusseus55 <odusseus.development@gmail.com>
2022-07-17 12:34:41 +0200

    fix: article card stories

--- src/features/articles/pages/DashboardPage/parts/ArticleCard/parts/ArticleCardComponent/_stories/ArticleComponent.stories.tsx
+++ src/features/articles/pages/DashboardPage/parts/ArticleCard/parts/ArticleCardComponent/_stories/ArticleComponent.stories.tsx
@@ -1,7 +1,10 @@
 import type { Meta, Story } from '@storybook/react/types-6-0'
 
 import doggo from '~/features/articles/fixtures/assets/doggo.webp'
-import type { ArticleDetailTye } from '~/features/articles/types'
+import {
+  createArticle,
+  createArticleDetail,
+} from '~/features/articles/types.fixtures'
 
 import type { Props } from '..'
 import { ArticleCardComponent } from '..'
@@ -17,15 +20,7 @@ const Template: Story<Props> = (args) => {
 
 export const Default = Template.bind({})
 Default.args = {
-  article: {
-    articleId: '15',
-    createdAt: '2022-07-15T12:50:25.23354',
-    imageId: '15',
-    lastUpdatedAt: '2022-07-15T12:50:25.23354',
-    perex:
-      'Clever, lively, courageous, entertaining: that’s the Dachshund. The Dachshund’s name comes from the German words for badger (dachs) and dog (hund)',
-    title: 'Dachshund Dog Breed Facts & Information',
-  },
+  article: createArticle(),
   imgURL: doggo,
-  articleDetail: { comments: [] } as ArticleDetailTye,
+  articleDetail: createArticleDetail(),
 }
